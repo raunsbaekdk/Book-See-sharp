065349fbb46810150651c57148b3b5ff2765555d
Stefan <Stefan.Weibel@gmail.com>
2014-05-20 12:33:23 +0200

    API
    API ACCESS FOR BUSSES

--- WebAPI/Controllers/BusController.cs
+++ WebAPI/Controllers/BusController.cs
@@ -4,9 +4,13 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using Models;
 
 namespace WebAPI.Models {
     public class BusController : ApiController {
-        static readonly IBusRespository Respository = new 
+        static readonly IBusRespository Respository = new BusRepository();
+        public IEnumerable<Bus> GetAllBusses() {
+            return Respository.GetAllBusses();
+        } 
     }
 }
