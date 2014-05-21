1f380f6d1101965ea7863b5c4a28a68e108e91bd
raunsbaekdk <mike@raunsbaek.dk>
2014-05-21 12:03:10 +0200

    Move PHP JS to ASP.NET JS

--- Book-See-sharp/Booking.aspx.designer.cs
+++ Book-See-sharp/Booking.aspx.designer.cs
@@ -11,14 +11,5 @@ namespace Book_See_sharp {
     
     
     public partial class Booking {
-        
-        /// <summary>
-        /// loginLoggedin control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Literal loginLoggedin;
     }
 }
